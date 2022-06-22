0b29cbbf9811e88511d13269e2f53e3f4a299d97
Scip <90598372+scipen@users.noreply.github.com>
2022-06-22 15:31:15 -0700

    add chessboardjsx

--- ui/src/tabs/About.tsx
+++ ui/src/tabs/About.tsx
@@ -1,10 +1,20 @@
+import Box from "@mui/material/Box";
 import ReactMarkdown from 'react-markdown';
+import Chessboard from 'chessboardjsx';
 
 export default function About() {
 
-  const md = `# zkApp: Zero-Knowledge dApp Boilerplate`;
+    const md = `# zkApp: Zero-Knowledge dApp Boilerplate`;
 
-  return (
-    <ReactMarkdown children={md}/>
-  );
+    return (
+        <Box
+            sx={{
+                "& .MuiTextField-root": { m: 1, width: "95%" },
+                width: "99%", margin: 'auto'
+            }}
+        >
+            <ReactMarkdown children={md}/>
+            <Chessboard position={{ e5: 'wK', e4: 'wP', e7: 'bK' }} />
+        </Box>
+    );
 }
